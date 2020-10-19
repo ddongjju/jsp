a6544da6553da36a3b9315c1ee7dbc54b7c23149
dongju <dongju5439@gmail.com>
2020-10-19 09:19:20 +0900

    실습  . pages 속성을 통해 페이지 내비게이션 생성 작업  . pageSize 파라미터 처리하기
    JSTL
     . 국제화 - bundle, message, setBundle
    
    실습
     . select box 변경시 언어 변경

--- src/test/java/kr/or/ddit/member/service/MemberServiceTest.java
+++ src/test/java/kr/or/ddit/member/service/MemberServiceTest.java
@@ -2,16 +2,20 @@ package kr.or.ddit.member.service;
 
 import static org.junit.Assert.*;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import kr.or.ddit.common.model.PageVo;
 import kr.or.ddit.member.model.MemberVo;
 
 public class MemberServiceTest {
-
+	private static final Logger logger = LoggerFactory.getLogger(MemberServiceTest.class);
 	@Test
 	public void getMemberTest() {
 		/*** Given ***/
@@ -59,6 +63,15 @@ public class MemberServiceTest {
 		/*** Then ***/
 		assertEquals(5, memberList.size());
 //		assertEquals(3, pages);
+	}
+	
+	@Test
+	public void localeListTest() {
+		Locale[] locales = SimpleDateFormat.getAvailableLocales();
+		for(Locale locale : locales) {
+			logger.debug("{}", locale);
+		}
+		
 	}
 
 }
