b5089ea7636080bc25e386f249399c69076c0d96
dongju <dongju5439@gmail.com>
2020-11-02 14:35:16 +0900

    spring 개요 spring 프로젝트 설정 spring  . IOC  . DI/DL
    실습
     . 스프링 컨테이너
     . bean 엘레멘트 속성

---
+++ src/main/java/kr/or/ddit/login/LoginServlet.java
@@ -0,0 +1,44 @@
+package kr.or.ddit.login;
+
+import java.io.IOException;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import kr.or.ddit.board.service.BoardServiceI;
+
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final Logger logger = LoggerFactory.getLogger(LoginServlet.class);
+       
+	@Resource(name="boardService")
+	private BoardServiceI boardService;
+	
+	@Override
+	public void init() throws ServletException {
+		
+	}
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		logger.debug("loginServlet doGet");
+		logger.debug("UNT_CD parameter : {} ", request.getParameter("UNT_CD"));
+		
+		request.getRequestDispatcher("/login.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
