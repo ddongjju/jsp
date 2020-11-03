20c3a7a548929db274250236dd7145ce7a8f761f
dongju <dongju5439@gmail.com>
2020-11-03 09:18:14 +0900

    스프링 설정  . component-scan  . java config  . java config - componentScan

--- src/main/java/kr/or/ddit/board/service/BoardService.java
+++ src/main/java/kr/or/ddit/board/service/BoardService.java
@@ -1,15 +1,21 @@
 package kr.or.ddit.board.service;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
 import kr.or.ddit.board.model.BoardVO;
 import kr.or.ddit.board.repository.BoardRepositoryI;
 
+@Service("boardService")
 public class BoardService implements BoardServiceI{
-   
-   private BoardRepositoryI boardRepository;
 
-   public BoardService() {
-      
-   }
+	@Resource(name = "boardRepository")
+	private BoardRepositoryI boardRepository;
+
+	public BoardService() {
+
+	}
    
    public BoardService(BoardRepositoryI boardRepository) {
       this.boardRepository = boardRepository;
